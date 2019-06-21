95b4ff31ab8d7ed687fbcf3dd59aedaed49db526
ntrobot2 <ntrobotshu@Ntrobot.local>
2019-06-21 11:38:39 +0900

    adds clicks per minute with interrupt implementation

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 import machine
 import time
 
+# queue / circular buffer
+pin_g = machine.Pin(2, machine.Pin.OUT)
+cp5s = [0 for i in range(0, 12)]
+
+count = 0
+
+
+def button_pressed(p: machine.Pin) -> None:
+    global count
+    count += 1
+    pin_g.on()
+    pin_g.off()
+
 
 def toggle(pin: machine.Pin) -> None:
     pin.value(not pin.value())
 
 
 def main():
-    pin_r = machine.Pin(1, machine.Pin.OUT)
-    pin_g = machine.Pin(2, machine.Pin.OUT)
-    pin_b = machine.Pin(3, machine.Pin.OUT)
+    global count
+    current = 0
+    pin_irover = machine.Pin(4, machine.Pin.IN)
+    pin_irover.irq(trigger=machine.Pin.IRQ_FALLING, handler=button_pressed)
     while 1:
-        toggle(pin_r)
-        time.sleep(0.5)
-        toggle(pin_g)
-        time.sleep(0.5)
-        toggle(pin_b)
-        time.sleep(0.3)
+        time.sleep(5)
+        cp5s[current] = count
+        count = 0
+        print("cp5s", cp5s[current], "cpm", sum(cp5s))
+        current = (current + 1) % 12
 
 
 if __name__ == '__main__':
     main()
+
